d680d5d36ecf9d660207692b0412c63cd5d94420
Codngwthubaid <codngwthubaid@gmail.com>
2024-12-04 17:00:30 +0530

    SetUp tailindconfig,metroconfig and working in index.tsx file

--- Ai-VideoUploading-App/tailwind.config.js
+++ Ai-VideoUploading-App/tailwind.config.js
@@ -1,6 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  // content: [
+  //   "./App.{js,jsx,ts,tsx}",
+  //   "./components/**/*.{js,jsx,ts,tsx}"
+  // ],
+  content: [
+    "./App.{js,jsx,ts,tsx}", // Adjust this path based on your project structure
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./screens/**/*.{js,jsx,ts,tsx}",
+    "./node_modules/nativewind/dist/**/*.js" // Include NativeWind
+  ],
   theme: {
     extend: {
       colors: {
