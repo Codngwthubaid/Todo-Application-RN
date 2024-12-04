2962a3c7f8d02d416af9c3af05d235c9731e5761
Codngwthubaid <codngwthubaid@gmail.com>
2024-12-04 23:42:12 +0530

    Building the Starting  Page

--- Ai-VideoUploading-App/app/index.tsx
+++ Ai-VideoUploading-App/app/index.tsx
@@ -1,12 +1,41 @@
 import React from 'react'
-import { StyleSheet, Text, View } from 'react-native'
-import { Link } from 'expo-router'
+import { Image, ScrollView, StyleSheet, Text, View } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { images } from "../constants/images"
 
 const App = () => {
   return (
-    <View className='w-full bg-black flex-1 justify-center items-center'>
-      <Link href="/home" className='text-blue-500 font-semibold'>Go to Profile</Link>   
-    </View>
+    <SafeAreaView className='bg-primary h-full'>
+      <ScrollView contentContainerStyle={{ height: "100%" }}>
+        <View className='flex justify-center items-center py-4'>
+          <Image
+            source={images.logo}
+            className='w-[130px] h-[85px]'
+            resizeMode='contain'
+          />
+
+          <Image
+            source={images.cards}
+            className='w-full max-w-[380px] h-[300px]'
+            resizeMode='contain'
+          />
+
+          <View className='relative mt-5'>
+            <Text className='text-4xl font-bold text-white text-center'>Discover Endless Possibilities with{" "}
+              <Text className='text-secondary-200'>Aora</Text>
+            </Text>
+
+            <Image
+              source={images.path}
+              className='w-[130px] h-[15px] absolute -bottom-2 -right-8'
+              resizeMode='contain'
+            />
+          </View>
+          <Text className='text-sm font-pregular mt-10 text-white text-center'>Where creativity meets innovations: embark on a journey of limitless exploration with Aora</Text>
+
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   )
 }
 
