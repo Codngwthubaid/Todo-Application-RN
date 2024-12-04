e612476f9506a65bad07e713f6f091684056a19b
Codngwthubaid <codngwthubaid@gmail.com>
2024-12-04 19:15:27 +0530

    woking on TabsLayout

--- Ai-VideoUploading-App/app/(tabs)/_layout.tsx
+++ Ai-VideoUploading-App/app/(tabs)/_layout.tsx
@@ -1,11 +1,43 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, Text, View, Image, ImageSourcePropType } from 'react-native'
 import React from 'react'
+import { Tabs, Redirect } from 'expo-router'
+import { icons } from "../../constants"
+
+
+const TabIcons = ({ icon, color, name, focused }) => {
+  return (
+    <View>
+      <Image
+        source={icon}
+        resizeMode='contain'
+        tintColor={color}
+        className='w-6 h-6'
+      />
+    </View>
+  )
+}
 
 const TabsLayout = () => {
   return (
-    <View>
-      <Text>TabsLayout</Text>
-    </View>
+    <>
+      <Tabs>
+        <Tabs.Screen
+          name='home'
+          options={{
+            title: "Home",
+            headerShown: false,
+            tabBarIcon: ({ color, focused }) => (
+              <TabIcons
+                icon={icons.home}
+                color={color}
+                name={"Home"}
+                focused={focused}
+              />
+            )
+          }}
+        />
+      </Tabs>
+    </>
   )
 }
 
--- Ai-VideoUploading-App/app/index.tsx
+++ Ai-VideoUploading-App/app/index.tsx
@@ -5,7 +5,7 @@ import { Link } from 'expo-router'
 const App = () => {
   return (
     <View className='w-full bg-black flex-1 justify-center items-center'>
-      <Link href="/profile" className='text-blue-500 font-semibold'>Go to Profile</Link>   
+      <Link href="/home" className='text-blue-500 font-semibold'>Go to Profile</Link>   
     </View>
   )
 }
