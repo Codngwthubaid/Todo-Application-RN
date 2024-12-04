a955eb7b29f4c966709064e9417cd5b03037ae32
Codngwthubaid <codngwthubaid@gmail.com>
2024-12-04 20:41:50 +0530

    The tabIcons successfully created

--- Ai-VideoUploading-App/app/(tabs)/_layout.tsx
+++ Ai-VideoUploading-App/app/(tabs)/_layout.tsx
@@ -1,46 +1,101 @@
-import { StyleSheet, Text, View, Image, ImageSourcePropType } from 'react-native'
-import React from 'react'
-import { Tabs, Redirect } from 'expo-router'
-import { icons } from "../../constants"
+import React from 'react';
+import { StyleSheet, View, Image, ImageSourcePropType, Text } from 'react-native';
+import { Tabs } from 'expo-router';
+import {icons} from "@/constants/icons"
 
+interface TabIconsProps {
+  icon: ImageSourcePropType;
+  color: string;
+  name: string;
+  focused: boolean;
+}
 
-const TabIcons = ({ icon, color, name, focused }) => {
+const TabIcons: React.FC<TabIconsProps> = ({ icon, color, name, focused }) => {
   return (
-    <View>
+    <View className='flex-1 items-center'>
       <Image
         source={icon}
         resizeMode='contain'
-        tintColor={color}
+        tintColor= {color}
         className='w-6 h-6'
       />
+      <Text className={`${focused ? "font-psemibold" : "font-pregular"} text-sm w-full`}>{name}</Text>
     </View>
-  )
-}
+  );
+};
 
-const TabsLayout = () => {
+const TabsLayout: React.FC = () => {
   return (
-    <>
-      <Tabs>
-        <Tabs.Screen
-          name='home'
-          options={{
-            title: "Home",
-            headerShown: false,
-            tabBarIcon: ({ color, focused }) => (
-              <TabIcons
-                icon={icons.home}
-                color={color}
-                name={"Home"}
-                focused={focused}
-              />
-            )
-          }}
-        />
-      </Tabs>
-    </>
-  )
-}
+    <Tabs 
+    screenOptions={{
+      tabBarShowLabel: false
+    }}
+    >
+      <Tabs.Screen
+        name='home'
+        options={{
+          title: "Home",
+          headerShown: false,
+          tabBarIcon: ({ color, focused }: { color: string; focused: boolean }) => (
+            <TabIcons
+              icon={icons.home}
+              color={color}
+              name={"Home"}
+              focused={focused}
+            />
+          )
+        }}   
+      />
+       <Tabs.Screen
+        name='bookmark'
+        options={{
+          title: "Bookmark",
+          headerShown: false,
+          tabBarIcon: ({ color, focused }: { color: string; focused: boolean }) => (
+            <TabIcons
+              icon={icons.bookmark}
+              color={color}
+              name={"Bookmark"}
+              focused={focused}
+            />
+          )
+        }}
+      />
+             <Tabs.Screen
+        name='create'
+        options={{
+          title: "Create",
+          headerShown: false,
+          tabBarIcon: ({ color, focused }: { color: string; focused: boolean }) => (
+            <TabIcons
+              icon={icons.plus}
+              color={color}
+              name={"Create"}
+              focused={focused}
+            />
+          )
+        }}
+      />
+             <Tabs.Screen
+        name='profile'
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ color, focused }: { color: string; focused: boolean }) => (
+            <TabIcons
+              icon={icons.profile}
+              color={color}
+              name={"Profile"}
+              focused={focused}
+            />
+          )
+        }}
+      />
+    </Tabs>
+  );
+};
 
-export default TabsLayout
+export default TabsLayout;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
+
