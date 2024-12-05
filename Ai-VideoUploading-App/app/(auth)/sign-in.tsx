7e25596c586820ae2fc3b94e7948e05960fec48d
Codngwthubaid <codngwthubaid@gmail.com>
2024-12-05 07:30:07 +0530

    Build the Basic layout for Sigin Page

--- Ai-VideoUploading-App/app/(auth)/sign-in.tsx
+++ Ai-VideoUploading-App/app/(auth)/sign-in.tsx
@@ -1,11 +1,21 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Image, SafeAreaView, ScrollView, StyleSheet, Text, View } from 'react-native'
+import { images } from "@/constants/images"
 
 const Signin = () => {
     return (
-        <View>
-            <Text>Signin</Text>
-        </View>
+        <SafeAreaView className='bg-primary h-full'>
+            <ScrollView>
+                <View className='w-full justify-center my-6 px-4 h-full'>
+                    <Image
+                        source={images.logo}
+                        className='w-[115px] h-[35px]'
+                        resizeMode='contain'
+                    />
+                    <Text className='text-3xl text-white font-psemibold mt-7'>Login to Aora</Text>
+                </View>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 
