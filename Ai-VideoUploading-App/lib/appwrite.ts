c7d002cef0d472a1776652960502ab8280e26c68
Codngwthubaid <codngwthubaid@gmail.com>
2024-12-05 11:05:09 +0530

    Add the Ids of appwrite and setUp the config of appwrite aswell

---
+++ Ai-VideoUploading-App/lib/appwrite.ts
@@ -0,0 +1,9 @@
+export const appwriteconfig = {
+    endpoint: "https://cloud.appwrite.io/v1",
+    project: "com.rn.ai-videouploading-app",
+    projectID: "6751336b00350cb1c90b",
+    databaseID: "675136430039b7e2c95f",
+    usersCollectionID: "675136a9003b953bdb7e",
+    videosCollectionID: "67513701003dabcd71e1",
+    stroageID: "67513974001040a9ad59"
+}
