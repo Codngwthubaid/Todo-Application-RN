7124ce6814e96be562d23a67412dae96bcd7150b
Codngwthubaid <codngwthubaid@gmail.com>
2024-12-05 07:22:53 +0530

    Make a basic structure for auth layout

--- Ai-VideoUploading-App/app/(auth)/_layout.tsx
+++ Ai-VideoUploading-App/app/(auth)/_layout.tsx
@@ -1,14 +1,26 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { Stack } from 'expo-router'
+import {StatusBar } from "expo-status-bar"
 
 const AuthLayout = () => {
   return (
-    <View>
-      <Text>AuthLayout</Text>
-    </View>
+  <Stack>
+    <Stack.Screen
+    name='Sign-In'
+    options={{
+      headerShown:false
+    }}
+    />
+    <Stack.Screen
+    name='Sign-Up'
+    options={{
+      headerShown:false
+    }}
+    />
+    <StatusBar style='light' backgroundColor='#161622'/>
+  </Stack>
   )
 }
 
 export default AuthLayout
-
-const styles = StyleSheet.create({})
